6dc9911fb87144428c1f5c7c405ba3f6df533dd0
Aunmag <aunmag@gmail.com>
2020-01-18 15:56:58 +0500

    Improved input

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,13 @@ mod utils;
 
 use crate::models::field::Field;
 use crate::models::sapper::Sapper;
-use crate::models::sapper::SapperAction;
+use termwiz::caps::Capabilities;
+use termwiz::input::InputEvent;
+use termwiz::input::KeyCode;
+use termwiz::input::KeyEvent;
+use termwiz::terminal::buffered::BufferedTerminal;
+use termwiz::terminal::new_terminal;
+use termwiz::terminal::Terminal;
 
 const FIELD_SIZE: usize = 30;
 const MINES_DENSITY: f64 = 0.25;
@@ -11,15 +17,55 @@ const MINES_DENSITY: f64 = 0.25;
 fn main() {
     let mut field = Field::new(FIELD_SIZE, MINES_DENSITY);
     let mut sapper = Sapper::new();
+    let mut buffer = BufferedTerminal::new(new_terminal(Capabilities::new_from_env().unwrap()).unwrap()).unwrap();
+    let mut render = true;
+
+    buffer.terminal().set_raw_mode().unwrap();
 
     loop {
-        field.render(&sapper);
+        if render {
+            buffer.flush().unwrap();
+            field.render(&sapper);
+            render = false;
+        }
 
-        if let Option::Some(action) = sapper.request_input(&field) {
-            match action {
-                SapperAction::Discover => field.discover(sapper.position),
-                SapperAction::Mark => field.mark(sapper.position),
-                SapperAction::Quit => break,
+        match buffer.terminal().poll_input(None) {
+            Ok(None) => {},
+            Ok(Some(input)) => {
+                match input {
+                    InputEvent::Key(KeyEvent {key: KeyCode::UpArrow, ..}) => {
+                        sapper._move(0, -1, &field);
+                        render = true;
+                    }
+                    InputEvent::Key(KeyEvent {key: KeyCode::DownArrow, ..}) => {
+                        sapper._move(0, 1, &field);
+                        render = true;
+                    }
+                    InputEvent::Key(KeyEvent {key: KeyCode::LeftArrow, ..}) => {
+                        sapper._move(-1, 0, &field);
+                        render = true;
+                    }
+                    InputEvent::Key(KeyEvent {key: KeyCode::RightArrow, ..}) => {
+                        sapper._move(1, 0, &field);
+                        render = true;
+                    }
+                    InputEvent::Key(KeyEvent {key: KeyCode::Char('m'), ..}) => {
+                        field.mark(sapper.position);
+                        render = true;
+                    }
+                    InputEvent::Key(KeyEvent {key: KeyCode::Char(' '), ..}) => {
+                        field.discover(sapper.position);
+                        render = true;
+                    }
+                    InputEvent::Key(KeyEvent {key: KeyCode::Escape, ..}) => {
+                        break;
+                    }
+                    _ => {}
+                }
+            }
+            Err(error) => {
+                println!("{:?}", error);
+                break;
             }
         }
     }
--- src/models/sapper.rs
+++ src/models/sapper.rs
@@ -1,7 +1,4 @@
 use crate::models::field::Field;
-use std::io::stdin;
-use std::io::stdout;
-use std::io::Write;
 
 pub struct Sapper {
     pub position: usize,
@@ -19,49 +16,10 @@ impl Sapper {
         };
     }
 
-    pub fn request_input(&mut self, field: &Field) -> Option<SapperAction> {
-        // TODO: Simplify
-
-        print!("Press a key then hit \"Enter\": ");
-
-        let mut s = String::new();
-        let _ = stdout().flush();
-        stdin().read_line(&mut s).expect("Did not enter a correct string");
-
-        if let Some('\n') = s.chars().next_back() {
-            s.pop();
-        }
-
-        if let Some('\r') = s.chars().next_back() {
-            s.pop();
-        }
-
-        let mut result = Option::None;
-
-        match s.as_str() {
-            "w" => self._move(0, -1, field),
-            "s" => self._move(0, 1, field),
-            "a" => self._move(-1, 0, field),
-            "d" => self._move(1, 0, field),
-            " " => result = Option::Some(SapperAction::Discover),
-            "m" => result = Option::Some(SapperAction::Mark),
-            "q" => result = Option::Some(SapperAction::Quit),
-            _ => (),
-        }
-
-        return result;
-    }
-
     pub fn _move(&mut self, x: i32, y: i32, field: &Field) {
         if let Option::Some(position) = field.move_position(self.position, x, y) {
             self.position = position;
         }
     }
 
-}
-
-pub enum SapperAction {
-    Discover,
-    Mark,
-    Quit,
 }
